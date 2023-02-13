39daf2f0048b27319fbcaab7d5c4fbc224831c04
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-12 21:34:17 -0500

    Add useState, response, event, and if else.

--- src/Weather.js
+++ src/Weather.js
@@ -1,69 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
 
 export default function Weather() {
-  let weatherData = {
-    city: "Buffalo",
-    temperature: "19°",
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-    humidity: 80,
-    wind: 10
-  };
-
-  return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autocomplete="off"
-            />
+    const [city, setCity] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [weather, setWeather] = useState({});
+    function displayWeather(response) {
+        setLoaded(true);
+        setWeather({
+          temperature: response.data.main.temp,
+          wind: response.data.wind.speed,
+          humidity: response.data.main.humidity,
+          icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        });
+      }
+    
+      function handleSubmit(event) {
+        event.preventDefault();
+        let apiKey = "50cfd6595523a7f69104e698dea7cff4";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayWeather);
+      }
+    
+      function updateCity(event) {
+        setCity(event.target.value);
+      }
+    
+      let form = (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            onChange={updateCity}
+          />
+          <button type="Submit">Search</button>
+        </form>
+      );
+    
+      if (loaded) {
+        return (
+          <div>
+            {form}
+            <ul>
+              <li>Temperature {Math.round(weather.temperature)}°C</li>
+              <li>Humidity {weather.humidity}%</li>
+              <li>Wind {Math.round(weather.wind)} km/h</li>
+              <li>
+                <img src={weather.icon} alt="Weather Icon" />
+              </li>
+            </ul>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img src={weatherData.imgUrl} alt="Clear" className="float-left" />
-            <div className="float-left">
-              <strong>{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Feels Like: 25°</li>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} mph </li>
-            <li>Air Pressure: 5 hPa</li>
-          </ul>
-        </div>
-      </div>
-      <div className="weather-forecast"></div>
-    </div>
-  );
-}
+        );
+      } else {
+        return form;
+      }
+    }
+    
