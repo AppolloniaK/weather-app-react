67433f4c23c9dffc70ddc70d023af39ccd6f5642
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-14 00:00:14 -0500

    Footer added

---
+++ src/Opensource.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="open-source">
+      <a
+        href="https://github.com/AppolloniaK/weather-app-react"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Open-source code
+      </a>
+      <br />
+      by Appollonia Khan.
+    </div>
+  );
+}
