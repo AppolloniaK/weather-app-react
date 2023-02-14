fc7c7d58b2e1fc3f9ab128e76381d2cfca539922
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-14 00:31:26 -0500

    Adding project site and github site to footer.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
     <div className="App">
        <h1>Weather App</h1>
       <Weather />
-      <Opensource />
+  <footer>
+  This project was coded by <a href="https://idyllic-cobbler-4d0550.netlify.app/" target="_blank">Appollonia Khan</a> and is <a
+        href="https://github.com/AppolloniaK/weather-app-react"
+        target="_blank"
+      >
+        Open-sourced</a> on GutHub
+  </footer>
       </div>
   );
 }
--- src/Opensource.js
+++ src/Opensource.js
@@ -3,15 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="open-source">
+      This projext was coded by Appollonia Khan and is 
       <a
         href="https://github.com/AppolloniaK/weather-app-react"
-        rel="noreferrer"
         target="_blank"
       >
-        Open-source code
+        Open-source on GutHub
       </a>
-      <br />
-      by Appollonia Khan.
     </div>
   );
 }
