2ba3535854af75fc918f0d8e3b4fdac07aef2943
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-16 23:10:58 -0500

    Added to Weather CSS, added to index css, and deleted Opensource page.

--- src/Weather.js
+++ src/Weather.js
@@ -4,65 +4,38 @@ import axios from "axios";
 
 
 export default function Weather() {
-    const [city, setCity] = useState("");
-    const [loaded, setLoaded] = useState(false);
-    const [weather, setWeather] = useState({});
-    
-    function displayWeather(response) {
-        setLoaded(true);
-        setWeather({
-          temperature: response.data.main.temp,
-          wind: response.data.wind.speed,
-          humidity: response.data.main.humidity,
-          icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-        });
-      }
-    
-      function handleSubmit(event) {
-        event.preventDefault();
-        let apiKey = "50cfd6595523a7f69104e698dea7cff4";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(displayWeather);
-      }
-    
-      function updateCity(event) {
-        setCity(event.target.value);
-      }
-    
-      let form = (
-        <form onSubmit={handleSubmit}>
-               <div className="row">
+   return (
+    <div className="Weather">
+        <form>     
+        <div className="row">
                 <div className="col-9">
           <input
             type="search"
             placeholder="Enter a city..."
             className="form-control"
-            onChange={updateCity}
           />
           </div>
-          <div className="col-3"></div>
+          <div className="col-3">
           <input type="submit" value="Search" className="btn btn-primary" />
           </div> 
-          </form>
-      );
-
-      if (loaded) {
-        return (
-            <div className="Weather">
-            <h2>Welcome to Weather!</h2>
-            {form}
-            <ul>
+          </div>
+          </form>     
+<h1>New York</h1>
+<ul>
+    <li>Wednesday 07:00</li>
+    <li>Mostly Cloudy</li>
+    </ul>
+    <div className="row">
+        <div className="col-6">
+                <img src={weather.icon} alt="Weather Icon" />°6
+        </div>
+    <ul>
               <li>Temperature {Math.round(weather.temperature)}°C</li>
               <li>Humidity {weather.humidity}%</li>
               <li>Wind {Math.round(weather.wind)} km/h</li>
-              <li>
-                <img src={weather.icon} alt="Weather Icon" />
-              </li>
             </ul>
-          </div>
-        );
-      } else {
-        return form;
-      };
+    </div>
+    </div>
+   );
     }
     
