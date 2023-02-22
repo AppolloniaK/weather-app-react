1252859981175681c3f474579856643492e58cd0
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-22 15:59:42 -0500

    added default city and setWeatherData

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className="container">
        <h1>Weather App</h1>
-      <Weather />
+      <Weather defaultCity="New York" />
   <footer>
   This project was coded by <a href="https://idyllic-cobbler-4d0550.netlify.app/" target="_blank">Appollonia Khan</a> and is <a
         href="https://github.com/AppolloniaK/weather-app-react"
--- src/Weather.js
+++ src/Weather.js
@@ -2,25 +2,23 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-
-export default function Weather(props) {
-    const [ready, setReady] = useState(false);
-    const [weatherData, setWeatherData] = useState({});
+export default function Weather() {
+    const [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
 console.log(response.data);
 setWeatherData({
+    ready: true,
     temperature: response.data.main.temp,
     humidity: response.data.main.humidity,
+    date: "Wednesday 07:00",
     description: response.data.weather[0].description,
-    icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    icon: "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png",
     wind: response.data.wind.speed,
-    city: response.data.name
+    city: response.data.name,
 });
-
-setReady(true);
     }
 
-    if (ready) {
+    if (weatherData.ready) {
    return (
     <div className="Weather">
         <form>     
@@ -43,13 +41,14 @@ setReady(true);
           </form>     
 <h1>{weatherData.city}</h1>
 <ul>
-    <li>Wednesday 07:00</li>
-    <li>{weatherData.description}</li>
+    <li>{weatherData.date}</li>
+    <li className="text-capitalize">{weatherData.description}</li>
     </ul>
     <div className="row mt-3">
         <div className="col-6">
             <div className="clearfix">
-                <img src={weatherData.icon} 
+                <img 
+                src={weatherData.icon} 
                 alt={weatherData.description}
                 className="float-left"
                 />
@@ -63,7 +62,7 @@ setReady(true);
         </div>
         <div className="col-6">
     <ul>
-              <li>Humidity {weatherData.humidity}%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
               <li>Wind: {Math.round(weatherData.wind)} km/h</li>
             </ul>
     </div>
