045480432043e92379511571a119903771b8d556
AppolloniaK <95185590+AppolloniaK@users.noreply.github.com>
2023-02-22 22:19:22 -0500

    create weather info

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 import axios from "axios";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
-
 setWeatherData({
     ready: true,
     temperature: response.data.main.temp,
@@ -19,10 +18,15 @@ setWeatherData({
 });
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        //search for a city
+    }
+
     if (weatherData.ready) {
    return (
     <div className="Weather">
-        <form>     
+        <form onSubmit={handleSUbmit}>     
         <div className="row">
                 <div className="col-9">
           <input
@@ -39,39 +43,8 @@ setWeatherData({
           />
           </div> 
           </div>
-          </form>     
-<h1>{weatherData.city}</h1>
-<ul>
-    <li>
-        <FormattedDate date={weatherData.date} />
-    </li>
-    <li className="text-capitalize">
-        {weatherData.description}
-        </li>
-    </ul>
-    <div className="row mt-3">
-        <div className="col-6">
-            <div className="clearfix">
-                <img 
-                src={weatherData.icon} 
-                alt={weatherData.description}
-                className="float-left"
-                />
-                <div className="float-left">
-                <span className="temperature">
-                    {Math.round(weatherData.temperature)}
-                    </span>
-                <span className="unit">°C</span>
-                </div>
-        </div>
-        </div>
-        <div className="col-6">
-    <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
-            </ul>
-    </div>
-    </div>
+          </form>  
+             <WeatherInfo data={weatherData} />
     </div>
    );
     } else {
